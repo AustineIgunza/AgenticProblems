8c8b7334357964da04ba10de71ff855434e3a951
AustineIgunza <Austine.igunza@strathmore.edu>
2025-05-24 21:12:44 +0300

    second commit

--- terminal_quest/main.py
+++ terminal_quest/main.py
@@ -3,3 +3,5 @@ def intro():
     print("Welcome,adventurer! You wake up in a dark forest with no memory.")
     print("Do you want to explore the forest or head towards the mountains")
     choice=input("Type 'forest' or 'mountains': ").strip().lower()
+    if choice =="forest":
+        forest()
